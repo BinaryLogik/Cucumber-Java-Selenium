843425be310fa3f1678edb3fefd61d3a253e417e
Jaye Thomas <jaye.thomas.se@gmail.com>
2020-10-20 00:06:32 -0700

    Removed tags=@Full so that command line tag specification works

--- src/test/java/io/cucumber/demo/RunCucumberTest.java
+++ src/test/java/io/cucumber/demo/RunCucumberTest.java
@@ -5,6 +5,6 @@ import io.cucumber.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty","html:reports/test-report.html"},tags="@Full")
+@CucumberOptions(plugin = {"pretty","html:reports/test-report.html"})
 public class RunCucumberTest {
 }
